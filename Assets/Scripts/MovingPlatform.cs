8d0f1fbec9da443930236c45667c87f8e4a9c081
Clara Gavinho <clara.gavinho@gmail.com>
2022-09-19 22:52:55 -0300

    Minor changes

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+[SerializeField] private Transform targetA, targetB; 
+     private float speed = 1f; //Change this to suit your game.
+     private bool switching = false;
+     void Update()
+     {
+             if (!switching)
+             {
+                     transform.position = Vector3.MoveTowards(transform.position, targetB.position, speed * Time.deltaTime); 
+             }
+             else if (switching)
+             {
+                     transform.position = Vector3.MoveTowards(transform.position, targetB.position, speed * Time.deltaTime); 
+             }
+             if (transform.position == targetB.position)
+             {
+                     switching = true;
+             }
+             else if (transform.position == targetB.position)
+             {
+                     switching = false;
+             }
+     }
+}
